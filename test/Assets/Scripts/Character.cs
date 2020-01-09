c657d26a1d2751e9326728c34e5fab3d6235984d
xinranma <maxinr@sheridancollege.ca>
2020-01-09 16:52:17 -0500

    character

---
+++ test/Assets/Scripts/Character.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public string c_name = "Xin";
+    public int health = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
